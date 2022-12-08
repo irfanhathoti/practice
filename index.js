dd696f7e9c5e462d3179cf3cc92411fda1e70ed8
irfan.hathoti <irfan.hathoti@htcinc.com>
2022-12-08 13:09:20 +0530

    'first'

---
+++ index.js
@@ -0,0 +1,6 @@
+// const getColor = () =>{
+//     const getElement = document.getElementById('color').value
+//     console.log(getElement)
+//     const span = document.getElementsByTagName('span')[0].style.color = getElement
+//     console.log(span)
+// }
